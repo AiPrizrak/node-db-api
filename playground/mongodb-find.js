e71fea3884b8be5c95ae3359d7ffed698ed2ef84
AiPrizrak <nnwarts@gmail.com>
2018-09-02 15:59:08 +0300

    Works on adding and fetching data from mongo

---
+++ playground/mongodb-find.js
@@ -0,0 +1,25 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err){
+       return console.log('Unable to connect to mongodb server');
+    }
+    console.log('Connected to mongodb server');
+    
+client.collection('Todos')
+    .find({_id: new ObjectID('5b8bb1c0697aa5265c535778')})
+    .toArray()
+    .then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+}, (err) => {
+    console.log('Unable to fetch todos', err);
+        });
+        
+client.collection('Users')
+.find({name: 'Andrew'})
+.count()
+.then((count) => console.log(`Todos Andrews count: ${count}`)
+, (err) => console.log('Unable to count Andrews'));
+client.close();
+});
