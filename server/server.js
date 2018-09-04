c8efa871c006c46cf6bb8cd4e3e69b7a2823a5a7
AiPrizrak <nnwarts@gmail.com>
2018-09-04 13:34:53 +0300

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/User');
+
 
 var app = express();
 
@@ -30,6 +32,22 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
       })
   });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    console.log(id);
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send('Invalid id');
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send('Not found');
+        }
+        res.status(200).send(todo);
+    }).catch((e) => {
+        return res.status(400).send('Todo not found');
+    })
+})
 
 app.listen(3001,() => {
     console.log('Started on port 3001');
